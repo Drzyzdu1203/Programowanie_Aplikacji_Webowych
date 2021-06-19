4d4604c26361f6de44905f16ac66c514dfff74a2
Mateusz Dryzga <mdrzyzdu@gmail.com>
2021-06-19 17:14:53 +0200

    working localstorage

--- Projekt4/ver.5/src/App.ts
+++ Projekt4/ver.5/src/App.ts
@@ -1,12 +1,14 @@
-import {Notes} from "./notes";
-import {Note} from "./note";
+//import { Notes } from "./notes";
+//import { Note } from "./note";
+import { IAppStorage } from "./IAppStorage";
 
 export default class App {
- 
   storageElement: number = 0;
+  notes: IAppStorage[] = [];
 
   constructor() {
     this.addNote();
+    this.notesInStorage();
   }
   addNote() {
     const clickAddButton = <HTMLInputElement>(
@@ -14,13 +16,41 @@ export default class App {
     );
     clickAddButton.addEventListener("click", (ev: Event) => this.addNewNote());
   }
-  async addNewNote(){
-    const noteTitle = <HTMLInputElement>document.getElementById('titleInput');
+  async addNewNote() {
+    const noteTitle = <HTMLInputElement>document.getElementById("titleInput");
     const title = noteTitle.value;
 
-    const noteDescription = <HTMLInputElement>document.getElementById('descriptionInput');
-    const description = noteDescription.value;  
+    const noteDescription = <HTMLInputElement>(
+      document.getElementById("descriptionInput")
+    );
+    const description = noteDescription.value;
+
+    const item: IAppStorage = {
+      id: this.storageElement + 1,
+      title: title,
+      description: description,
+      date: new Date().toDateString(),
+    };
+    this.saveData(item);
+  }
+  saveData(addNewNote: any) {
+    this.storageElement++;
+    this.notes.push(addNewNote);
+    localStorage.setItem(
+      "note" + this.storageElement,
+      JSON.stringify(addNewNote)
+    );
+    const result = JSON.parse(
+      localStorage.getItem("note" + this.storageElement)
+    );
+    console.log(result);
+  }
+
+  notesInStorage() {
+    const notesStorage = localStorage.length;
+    for (let i = 1; i < notesStorage; i++) {
+      localStorage.getItem("notes" + i);
+      this.addNewNote();
+    }
   }
 }
-
-
